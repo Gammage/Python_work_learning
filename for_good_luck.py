189153a8f23f56327d4327f29651425dc846b9da
benjamin <benjamin.gammage@gmail.com>
2025-10-21 02:06:27 +0100

    back to basics, learning syntax properly.

---
+++ for_good_luck.py
@@ -0,0 +1,10 @@
+# print("hello world")
+
+message = "hello python world"
+print (message)
+
+message = "hello python crash course world!"
+print (message)
+
+message = "hello python crash course reader"
+print (message)
