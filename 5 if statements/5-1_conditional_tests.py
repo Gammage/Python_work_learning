af6e5e1382d77a5038bb84990c8ba0beee08dd74
benjamin <benjamin.gammage@gmail.com>
2025-12-26 22:40:58 +0000

    fixed a confusing print

--- 5 if statements/5-1_conditional_tests.py
+++ 5 if statements/5-1_conditional_tests.py
@@ -6,7 +6,7 @@ if jungler == 'annie':
     print('true')
 
 if jungler != 'annie':
-    print('annie is not a jungler')
+    print(f"{jungler} is not annie")
     
 if jungler == 'shaco':
     print('shaco is a jungler')
